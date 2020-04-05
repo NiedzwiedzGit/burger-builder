146ce6196ba5c6dd2e024fa86fbd21bc4354ad77
Igor Skliarsky <shkliarskiyigor@gmail.com>
2020-04-05 18:53:44 +0200

    Improving Performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,38 @@
-import React from "react";
-import Aux from '../../../hoc/Auxiliary';
+import React, { Component } from "react";
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Button from '../../UI/Button/Button';
-const orderSummary = (props) => {
-    const indredientSummary = Object.keys(props.ingredients).map(igKey => {
+
+class OrderSummary extends Component {
+    // componentWillUpdate() {
+    //     console.log('[OrderSummary] willUpdate');
+    // }
+
+    render() {
+        const indredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+            return (
+                <li key={igKey}>
+                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>
+                    :{this.props.ingredients[igKey]}
+                </li>)
+        });
+
         return (
-            <li key={igKey}>
-                <span style={{ textTransform: 'capitalize' }}>{igKey}</span>
-                :{props.ingredients[igKey]}
-            </li>)
-    });
-
-    return (
-        <Aux>
-            <h3>Your order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {indredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    );
-
+            <Aux>
+                <h3>Your order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {indredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button
+                    btnType="Danger"
+                    clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button
+                    btnType="Success"
+                    clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux >
+        );
+    }
 };
-
-export default orderSummary;
+export default OrderSummary;
