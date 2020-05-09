1d1f38995a27aec9655ff8194abfc51fde012e9c
Igor Skliarsky <shkliarskiyigor@gmail.com>
2020-05-09 17:33:45 +0200

    Logging User Out

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,6 +22,19 @@ export const authFail = (error) => {
     };
 }
 
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    };
+};
+export const checkAuthTimeout = (experationTime) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout());
+        }, experationTime * 1000);
+    };
+}
+
 export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
@@ -38,6 +51,7 @@ export const auth = (email, password, isSignup) => {
             .then(response => {
                 console.log(response);
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn))
             })
             .catch(err => {
                 dispatch(authFail(err.response.data.error));
