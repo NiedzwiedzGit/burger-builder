8cd5a60c4c68b85bb7eaed2af6b32032a0ff8800
Igor Skliarsky <shkliarskiyigor@gmail.com>
2020-05-09 14:46:49 +0200

    Storing the Token

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -7,10 +7,11 @@ export const authStart = () => {
     };
 }
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
     return {
         type: actionTypes.AUTH_SUCCESS,
-        authData: authData
+        idToken: token,
+        userId: userId
     };
 }
 
@@ -36,7 +37,7 @@ export const auth = (email, password, isSignup) => {
         axios.post(url, authData)
             .then(responce => {
                 console.log(responce);
-                dispatch(authSuccess(responce.data));
+                dispatch(authSuccess(responce.data.idToken, responce.data.localId));
             })
             .catch(err => {
                 console.log(err);
