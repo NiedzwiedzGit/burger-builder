35610ebbb7486fe0ade966af624e86f3e68099da
Igor Skliarsky <shkliarskiyigor@gmail.com>
2020-05-26 14:15:08 +0200

    Handling Authentication with a Saga

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -44,31 +44,13 @@ export const checkAuthTimeout = (experationTime) => {
 }
 
 export const auth = (email, password, isSignup) => {
-    return dispatch => {
-        dispatch(authStart());
-        const authData = {
-            email: email,
-            password: password,
-            returnSecureToken: true
-        };
-        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBH2H77-aKMk6KUQQKBfDc9kp3MuO4z-f0';
-        if (!isSignup) {
-            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBH2H77-aKMk6KUQQKBfDc9kp3MuO4z-f0';
-        }
-        axios.post(url, authData)
-            .then(response => {
-                const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
-                localStorage.setItem('token', response.data.idToken);
-                localStorage.setItem('expirationDate', expirationDate);
-                localStorage.setItem('userId', response.data.localId);
-                dispatch(authSuccess(response.data.idToken, response.data.localId));
-                dispatch(checkAuthTimeout(response.data.expiresIn))
-            })
-            .catch(err => {
-                dispatch(authFail(err.response.data.error));
-            });
+    return {
+        type: actionTypes.AUTH_USER,
+        email: email,
+        password: password,
+        isSignup: isSignup
     };
-}
+};
 
 export const setAuthRedirectPath = (path) => {
     return {
